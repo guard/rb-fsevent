cb5f46224319f3b24db0ad9ad55460dc8fb6c8d9
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-10-26 20:55:16 +0200

    Added 1.8.6 support

--- lib/rb-fsevent/fsevent.rb
+++ lib/rb-fsevent/fsevent.rb
@@ -25,7 +25,7 @@ private
   end
   
   def launch_bin
-      @pipe = IO.popen("#{bin_path}/fsevent_watch #{path.shellescape}")
+    @pipe = IO.popen("#{bin_path}/fsevent_watch #{shellescape(path)}")
   end
   
   def listen
@@ -37,6 +37,24 @@ private
     end
   rescue Interrupt
     stop
+  end
+  
+  # for Ruby 1.8.6  support
+  def shellescape(str)
+    # An empty argument will be skipped, so return empty quotes.
+    return "''" if str.empty?
+    
+    str = str.dup
+    
+    # Process as a single byte sequence because not all shell
+    # implementations are multibyte aware.
+    str.gsub!(/([^A-Za-z0-9_\-.,:\/@\n])/n, "\\\\\\1")
+    
+    # A LF cannot be escaped with a backslash because a backslash + LF
+    # combo is regarded as line continuation and simply ignored.
+    str.gsub!(/\n/, "'\n'")
+    
+    return str
   end
   
 end
