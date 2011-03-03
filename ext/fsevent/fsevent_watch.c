7d70da3def83ff8d90943e8aee0ad6a375566c77
Travis Tilley <ttilley@gmail.com>
2011-03-03 01:18:30 -0500

    parse commandline options into a reusable data structure, resolve paths so as to avoid inconsistencies, enable setting the FSEventStreamEventId, enable setting the latency, enable setting the kFSEventStreamCreateFlagNoDefer flag, enable setting the currently unhandled kFSEventStreamCreateFlagWatchRoot flag (please ignore this one right now), allow for a default path of the current directory when no path has been passed in to fsevent_watch, fix a completely negligible potential memory leak warning from clang, output a fair amount of debug info when compiled with DEBUG defined

--- ext/fsevent/fsevent_watch.c
+++ ext/fsevent/fsevent_watch.c
@@ -1,47 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 #include <CoreServices/CoreServices.h>
 
-void callback(ConstFSEventStreamRef streamRef,
-  void *clientCallBackInfo,
-  size_t numEvents,
-  void *eventPaths,
-  const FSEventStreamEventFlags eventFlags[],
-  const FSEventStreamEventId eventIds[]
-) {
-  // Print modified dirs
-  int i;
+
+// Default flags for FSEventStreamCreate
+// Despite the fact that this binary doesn't (at least currently) perform any IO
+// other than to stderr/stdout, I figure the "ignore self" option is a fairly
+// sane default for this.
+const FSEventStreamCreateFlags
+FWDefaultFSEventStreamCreateFlags = kFSEventStreamCreateFlagIgnoreSelf;
+
+// Structure for storing metadata parsed from the commandline
+typedef struct _cli_settings_t {
+  CFMutableArrayRef           paths;
+  FSEventStreamEventId        sinceWhen;
+  CFTimeInterval              latency;
+  FSEventStreamCreateFlags    flags;
+} cli_settings_t;
+
+
+// Prototypes
+void          append_path(cli_settings_t *cli_settings, const char *path);
+void          parse_cli_settings(int argc,
+                                 const char *argv[],
+                                 cli_settings_t *cli_settings);
+static void   callback(ConstFSEventStreamRef streamRef,
+                       void *clientCallBackInfo,
+                       size_t numEvents,
+                       void *eventPaths,
+                       const FSEventStreamEventFlags eventFlags[],
+                       const FSEventStreamEventId eventIds[]);
+
+
+// Resolve a path and append it to the CLI settings structure
+// The FSEvents API will, internally, resolve paths using a similar scheme.
+// Performing this ahead of time makes things less confusing, IMHO.
+void append_path(cli_settings_t *cli_settings, const char *path)
+{
+  char fullPath[PATH_MAX];
+
+  if (realpath(path, fullPath) == NULL) {
+    if (path[0] != '/') {
+      size_t len;
+      getcwd(fullPath, sizeof(fullPath));
+      len = strlen(fullPath);
+      fullPath[len] = '/';
+      strlcpy(&fullPath[len + 1], path, sizeof(fullPath) - (len + 1));
+    } else {
+      strlcpy(fullPath, path, sizeof(fullPath));
+    }
+  }
+
+  CFStringRef pathRef = CFStringCreateWithCString(kCFAllocatorDefault,
+                                                  fullPath,
+                                                  kCFStringEncodingUTF8);
+  CFArrayAppendValue(cli_settings->paths, pathRef);
+  CFRelease(pathRef);
+}
+
+// Parse commandline settings
+void parse_cli_settings(int argc,
+                        const char *argv[],
+                        cli_settings_t *cli_settings)
+{
+  memset(cli_settings, 0, sizeof(cli_settings_t));
+
+  cli_settings->sinceWhen = kFSEventStreamEventIdSinceNow;
+  cli_settings->latency = 0.5;
+  cli_settings->flags = FWDefaultFSEventStreamCreateFlags;
+  cli_settings->paths = CFArrayCreateMutable(NULL, 0, &kCFTypeArrayCallBacks);
+
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "--since_when") == 0) {
+      cli_settings->sinceWhen = strtoull(argv[++i], NULL, 0);
+    } else if (strcmp(argv[i], "--latency") == 0) {
+      cli_settings->latency = strtod(argv[++i], NULL);
+    } else if (strcmp(argv[i], "--no-defer") == 0) {
+      cli_settings->flags |= kFSEventStreamCreateFlagNoDefer;
+    } else if (strcmp(argv[i], "--watch-root") == 0) {
+      cli_settings->flags |= kFSEventStreamCreateFlagWatchRoot;
+    } else {
+      append_path(cli_settings, argv[i]);
+    }
+  }
+
+  if (CFArrayGetCount(cli_settings->paths) == 0) {
+    append_path(cli_settings, ".");
+  }
+
+#ifdef DEBUG
+  fprintf(stderr, "cli_settings->since_when   %llu\n", cli_settings->sinceWhen);
+  fprintf(stderr, "cli_settings->latency      %f\n", cli_settings->latency);
+  fprintf(stderr, "cli_settings->flags        %#0.8x\n", cli_settings->flags);
+  fprintf(stderr, "cli_settings->paths\n");
+
+  long numpaths = CFArrayGetCount(cli_settings->paths);
+
+  for (long i = 0; i < numpaths; i++) {
+    char path[PATH_MAX];
+    CFStringGetCString(CFArrayGetValueAtIndex(cli_settings->paths, i),
+                       path,
+                       PATH_MAX,
+                       kCFStringEncodingUTF8);
+    fprintf(stderr, "  %s\n", path);
+  }
+
+  fprintf(stderr, "\n\n");
+  fflush(stderr);
+#endif
+}
+
+static void callback(ConstFSEventStreamRef streamRef,
+                     void *clientCallBackInfo,
+                     size_t numEvents,
+                     void *eventPaths,
+                     const FSEventStreamEventFlags eventFlags[],
+                     const FSEventStreamEventId eventIds[])
+{
   char **paths = eventPaths;
-  for (i = 0; i < numEvents; i++) {
+
+  for (size_t i = 0; i < numEvents; i++) {
     printf("%s", paths[i]);
     printf(":");
   }
+
   printf("\n");
   fflush(stdout);
 }
 
-int main (int argc, const char * argv[]) {
-  // Collect arguments as paths to watch
-  CFMutableArrayRef pathsToWatch = CFArrayCreateMutable(NULL, argc-1, NULL);
-  int i;
-  for(i=1; i<argc; i++) {
-    CFArrayAppendValue(pathsToWatch, CFStringCreateWithCString(kCFAllocatorDefault, argv[i], kCFStringEncodingUTF8));
-  }
-
-  // Create event stream
-  void *callbackInfo = NULL;
+int main(int argc, const char *argv[])
+{
+  cli_settings_t _cli_settings, *settings = &_cli_settings;
+  parse_cli_settings(argc, argv, settings);
+  FSEventStreamContext *context = NULL;
   FSEventStreamRef stream;
-  CFAbsoluteTime latency = 0.5;
-  stream = FSEventStreamCreate(
-    kCFAllocatorDefault,
-    callback,
-    callbackInfo,
-    pathsToWatch,
-    kFSEventStreamEventIdSinceNow,
-    latency,
-    kFSEventStreamCreateFlagNone
-  );
+  stream = FSEventStreamCreate(kCFAllocatorDefault,
+                               callback,
+                               context,
+                               settings->paths,
+                               settings->sinceWhen,
+                               settings->latency,
+                               settings->flags);
 
-  // Add stream to run loop
-  FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+#ifdef DEBUG
+  FSEventStreamShow(stream);
+  fprintf(stderr, "\n\n");
+  fflush(stderr);
+#endif
+
+  FSEventStreamScheduleWithRunLoop(stream,
+                                   CFRunLoopGetCurrent(),
+                                   kCFRunLoopDefaultMode);
   FSEventStreamStart(stream);
   CFRunLoopRun();
 
