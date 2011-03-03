6e638b936bc3d4176456de79c85df52386d8d15c
Travis Tilley <ttilley@gmail.com>
2011-03-03 18:27:39 -0500

    add test for executability of FSEvent.watcher_path, fix apostrophe test to not fail if the newfile already exists pre-test (and instead removing it), fix other newfile test is the same way

--- spec/rb-fsevent/fsevent_spec.rb
+++ spec/rb-fsevent/fsevent_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe FSEvent do
-  
+
   before(:each) do
     @results = []
     @fsevent = FSEvent.new
@@ -9,31 +9,37 @@ describe FSEvent do
       @results += paths
     end
   end
-  
+
+  it "should have a watcher_path that resolves to an executable file" do
+    File.exists?(FSEvent.watcher_path).should be_true
+    File.executable?(FSEvent.watcher_path).should be_true
+  end
+
   it "should work with path with an apostrophe" do
-    custom_path =  @fixture_path.join("custom 'path")
+    custom_path = @fixture_path.join("custom 'path")
+    file = custom_path.join("newfile.rb").to_s
+    File.delete file if File.exists? file
     @fsevent.watch custom_path.to_s do |paths|
       @results += paths
     end
-    file = custom_path.join("newfile.rb")
-    File.exists?(file).should be_false
+    @fsevent.paths.should == ["#{custom_path}"]
     run
     FileUtils.touch file
     stop
     File.delete file
     @results.should == [custom_path.to_s + '/']
   end
-  
+
   it "should catch new file" do
     file = @fixture_path.join("newfile.rb")
-    File.exists?(file).should be_false
+    File.delete file if File.exists? file
     run
     FileUtils.touch file
     stop
     File.delete file
     @results.should == [@fixture_path.to_s + '/']
   end
-  
+
   it "should catch file update" do
     file = @fixture_path.join("folder1/file1.txt")
     File.exists?(file).should be_true
@@ -42,7 +48,7 @@ describe FSEvent do
     stop
     @results.should == [@fixture_path.join("folder1/").to_s]
   end
-  
+
   it "should catch files update" do
     file1 = @fixture_path.join("folder1/file1.txt")
     file2 = @fixture_path.join("folder1/folder2/file2.txt")
@@ -54,16 +60,16 @@ describe FSEvent do
     stop
     @results.should == [@fixture_path.join("folder1/").to_s, @fixture_path.join("folder1/folder2/").to_s]
   end
-  
+
   def run
     sleep 1
     Thread.new { @fsevent.run }
     sleep 1
   end
-  
+
   def stop
     sleep 1
     @fsevent.stop
   end
-  
+
 end
