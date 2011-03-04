2ca7eaee8d723d220800dbf307eb58eb20f3a1fc
Travis Tilley <ttilley@gmail.com>
2011-03-03 21:36:40 -0500

    make settings global as they essentially are anyways

--- ext/fsevent/fsevent_watch.c
+++ ext/fsevent/fsevent_watch.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <string.h>
 
-#include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 
 
@@ -12,31 +11,35 @@ const FSEventStreamCreateFlags
 FWDefaultFSEventStreamCreateFlags = kFSEventStreamCreateFlagNone;
 
 // Structure for storing metadata parsed from the commandline
-typedef struct _cli_settings_t {
+typedef struct _global_settings_t {
   CFMutableArrayRef           paths;
   FSEventStreamEventId        sinceWhen;
   CFTimeInterval              latency;
   FSEventStreamCreateFlags    flags;
-} cli_settings_t;
+} global_settings_t;
 
 
 // Prototypes
-void          append_path(cli_settings_t *cli_settings, const char *path);
-void          parse_cli_settings(int argc,
-                                 const char *argv[],
-                                 cli_settings_t *cli_settings);
-static void   callback(ConstFSEventStreamRef streamRef,
-                       void *clientCallBackInfo,
-                       size_t numEvents,
-                       void *eventPaths,
-                       const FSEventStreamEventFlags eventFlags[],
-                       const FSEventStreamEventId eventIds[]);
+static void         append_path(const char *path);
+static inline void  parse_cli_settings(int argc,
+                                       const char *argv[]);
+static inline void  init_global_settings();
+static void         callback(ConstFSEventStreamRef streamRef,
+                             void *clientCallBackInfo,
+                             size_t numEvents,
+                             void *eventPaths,
+                             const FSEventStreamEventFlags eventFlags[],
+                             const FSEventStreamEventId eventIds[]);
+
+
+// define global settings structure
+global_settings_t _global_settings, *settings = &_global_settings;
 
 
 // Resolve a path and append it to the CLI settings structure
 // The FSEvents API will, internally, resolve paths using a similar scheme.
 // Performing this ahead of time makes things less confusing, IMHO.
-void append_path(cli_settings_t *cli_settings, const char *path)
+static void append_path(const char *path)
 {
 #ifdef DEBUG
   fprintf(stderr, "\n");
@@ -79,59 +82,49 @@ void append_path(cli_settings_t *cli_settings, const char *path)
   CFStringRef pathRef = CFStringCreateWithCString(kCFAllocatorDefault,
                                                   fullPath,
                                                   kCFStringEncodingUTF8);
-  CFArrayAppendValue(cli_settings->paths, pathRef);
+  CFArrayAppendValue(settings->paths, pathRef);
   CFRelease(pathRef);
 }
 
 // Parse commandline settings
-void parse_cli_settings(int argc,
-                        const char *argv[],
-                        cli_settings_t *cli_settings)
+static inline void parse_cli_settings(int argc,
+                                      const char *argv[])
 {
-  memset(cli_settings, 0, sizeof(cli_settings_t));
-
-  cli_settings->sinceWhen = 0xFFFFFFFFFFFFFFFFULL;
-  cli_settings->latency = 0.5;
-  cli_settings->flags = FWDefaultFSEventStreamCreateFlags;
-  cli_settings->paths = CFArrayCreateMutable(NULL,
-                                             (CFIndex)0,
-                                             &kCFTypeArrayCallBacks);
-
   for (int i = 1; i < argc; i++) {
     if (strcmp(argv[i], "--since_when") == 0) {
-      cli_settings->sinceWhen = strtoull(argv[++i], NULL, 0);
+      settings->sinceWhen = strtoull(argv[++i], NULL, 0);
     } else if (strcmp(argv[i], "--latency") == 0) {
-      cli_settings->latency = strtod(argv[++i], NULL);
+      settings->latency = strtod(argv[++i], NULL);
     } else if (strcmp(argv[i], "--no-defer") == 0) {
-      cli_settings->flags |= kFSEventStreamCreateFlagNoDefer;
+      settings->flags |= kFSEventStreamCreateFlagNoDefer;
     } else if (strcmp(argv[i], "--watch-root") == 0) {
-      cli_settings->flags |= kFSEventStreamCreateFlagWatchRoot;
+      settings->flags |= kFSEventStreamCreateFlagWatchRoot;
     } else if (strcmp(argv[i], "--ignore-self") == 0) {
 #if MAC_OS_X_VERSION_MAX_ALLOWED >= 1060
-      cli_settings->flags |= kFSEventStreamCreateFlagIgnoreSelf;
+      settings->flags |= kFSEventStreamCreateFlagIgnoreSelf;
 #else
       fprintf(stderr, "MacOSX10.6.sdk is required for --ignore-self\n");
 #endif
     } else {
-      append_path(cli_settings, argv[i]);
+      append_path(argv[i]);
     }
   }
 
-  if (CFArrayGetCount(cli_settings->paths) == 0) {
-    append_path(cli_settings, ".");
+  if (CFArrayGetCount(settings->paths) == 0) {
+    append_path(".");
   }
 
 #ifdef DEBUG
-  fprintf(stderr, "cli_settings->since_when   %llu\n", cli_settings->sinceWhen);
-  fprintf(stderr, "cli_settings->latency      %f\n", cli_settings->latency);
-  fprintf(stderr, "cli_settings->flags        %#.8x\n", cli_settings->flags);
-  fprintf(stderr, "cli_settings->paths\n");
+  fprintf(stderr, "settings->since_when   %llu\n", settings->sinceWhen);
+  fprintf(stderr, "settings->latency      %f\n", settings->latency);
+  fprintf(stderr, "settings->flags        %#.8x\n", settings->flags);
+  fprintf(stderr, "settings->paths\n");
 
-  long numpaths = CFArrayGetCount(cli_settings->paths);
+  long numpaths = CFArrayGetCount(settings->paths);
 
   for (long i = 0; i < numpaths; i++) {
     char path[PATH_MAX];
-    CFStringGetCString(CFArrayGetValueAtIndex(cli_settings->paths, i),
+    CFStringGetCString(CFArrayGetValueAtIndex(settings->paths, i),
                        path,
                        PATH_MAX,
                        kCFStringEncodingUTF8);
@@ -174,10 +167,21 @@ static void callback(ConstFSEventStreamRef streamRef,
   fflush(stdout);
 }
 
+static inline void init_global_settings(){
+  memset(settings, 0, sizeof(global_settings_t));
+
+  settings->sinceWhen = 0xFFFFFFFFFFFFFFFFULL;
+  settings->latency = 0.5;
+  settings->flags = FWDefaultFSEventStreamCreateFlags;
+  settings->paths = CFArrayCreateMutable(NULL,
+                                         (CFIndex)0,
+                                         &kCFTypeArrayCallBacks);
+}
+
 int main(int argc, const char *argv[])
 {
-  cli_settings_t _cli_settings, *settings = &_cli_settings;
-  parse_cli_settings(argc, argv, settings);
+  init_global_settings();
+  parse_cli_settings(argc, argv);
 
   FSEventStreamContext *context = NULL;
   FSEventStreamRef stream;
