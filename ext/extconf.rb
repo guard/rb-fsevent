57cb75cab2a91148c6f50bf5dc734aaf9d771663
Travis Tilley <ttilley@gmail.com>
2011-03-03 20:33:45 -0500

    fix spelling errors, output the compile command being used to stderr

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,15 +5,15 @@ create_makefile('none')
 if `uname -s`.chomp != 'Darwin'
   puts "Warning! Only Darwin (Mac OS X) systems are supported, nothing will be compiled"
 else
-  gem_root      = File.expand_path(File.join('..'))
-  darwin_verion = `uname -r`.to_i
-  sdk_verion    = { 9 => '10.5', 10 => '10.6', 11 => '10.7' }[darwin_verion]
+  gem_root       = File.expand_path(File.join('..'))
+  darwin_version = `uname -r`.to_i
+  sdk_version    = { 9 => '10.5', 10 => '10.6', 11 => '10.7' }[darwin_version]
 
-  raise "Only Darwin systems greather than 8 (Mac OS X 10.5+) are supported" unless sdk_verion
+  raise "Only Darwin systems greater than 8 (Mac OS X 10.5+) are supported" unless sdk_version
 
   core_flags = %W{
-    -isysroot /Developer/SDKs/MacOSX#{sdk_verion}.sdk
-    -mmacosx-version-min=#{sdk_verion} -mdynamic-no-pic -std=gnu99
+    -isysroot /Developer/SDKs/MacOSX#{sdk_version}.sdk
+    -mmacosx-version-min=#{sdk_version} -mdynamic-no-pic -std=gnu99
   }
 
   cflags = core_flags + %w{-Os -pipe}
@@ -36,9 +36,13 @@ else
     -D DEBUG=true
   } if ENV['FWDEBUG'] == "true"
 
+  compile_command = "CFLAGS='#{cflags.join(' ')} #{wflags.join(' ')}' /usr/bin/gcc #{gcc_opts.join(' ')} -o '#{gem_root}/bin/fsevent_watch' fsevent/fsevent_watch.c"
+
+  STDERR.puts(compile_command)
+
   # Compile the actual fsevent_watch binary
   system "mkdir -p #{File.join(gem_root, 'bin')}"
-  system "CFLAGS='#{cflags.join(' ')} #{wflags.join(' ')}' /usr/bin/gcc #{gcc_opts.join(' ')} -o '#{gem_root}/bin/fsevent_watch' fsevent/fsevent_watch.c"
+  system compile_command
 
   unless File.executable?("#{gem_root}/bin/fsevent_watch")
     raise "Compilation of fsevent_watch failed (see README)"
