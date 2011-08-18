7d0c69cb8cbbe1a10bdf15f18722fdcad90b8e55
Travis Tilley <ttilley@gmail.com>
2011-08-17 22:04:53 -0400

    i'm tempted to burn main.c with fire, so lets contain the scope of any flammables

--- fsevent_watch/main.c
+++ fsevent_watch/main.c
@@ -1,5 +1,30 @@
-#include "fsevent_watch.h"
+#include "common.h"
 #include "cli.h"
+
+// Structure for storing metadata parsed from the commandline
+static struct {
+  FSEventStreamEventId            sinceWhen;
+  CFTimeInterval                  latency;
+  FSEventStreamCreateFlags        flags;
+  CFMutableArrayRef               paths;
+  enum FSEventWatchOutputFormat   format;
+} config = {
+  (UInt64) kFSEventStreamEventIdSinceNow,
+  (double) 0.3,
+  (UInt32) kFSEventStreamCreateFlagNone,
+  NULL,
+  kFSEventWatchOutputFormatClassic
+};
+
+// Prototypes
+static void         append_path(const char *path);
+static inline void  parse_cli_settings(int argc, const char *argv[]);
+static void         callback(FSEventStreamRef streamRef,
+                             void *clientCallBackInfo,
+                             size_t numEvents,
+                             void *eventPaths,
+                             const FSEventStreamEventFlags eventFlags[],
+                             const FSEventStreamEventId eventIds[]);
 
 // Resolve a path and append it to the CLI settings structure
 // The FSEvents API will, internally, resolve paths using a similar scheme.
