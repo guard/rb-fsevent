2be35f19fc6691faef5b28d702093ddd2b4834ea
Travis Tilley <ttilley@gmail.com>
2017-06-01 15:00:57 -0400

    update ruby code for calling fsevent_watch with OTNetstrings output format this should fix any bugs with paths that contain colons or newlines

--- lib/rb-fsevent/fsevent.rb
+++ lib/rb-fsevent/fsevent.rb
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 
+require 'otnetstring'
+
 class FSEvent
   class << self
     class_eval <<-END
@@ -40,9 +42,28 @@ class FSEvent
     # please note the use of IO::select() here, as it is used specifically to
     # preserve correct signal handling behavior in ruby 1.8.
     while @running && IO::select([@pipe], nil, nil, nil)
-      if line = @pipe.readline
-        modified_dir_paths = line.split(':').select { |dir| dir != "\n" }
-        callback.call(modified_dir_paths)
+      # managing the IO ourselves allows us to be careful and never pass an
+      # incomplete message to OTNetstring.parse()
+      until @pipe.eof?
+        message = ""
+        length = ""
+
+        byte = @pipe.read(1)
+        while byte =~ /\d/
+          length << byte
+          byte = @pipe.read(1)
+        end
+        length = Integer(length, 10)
+        type = byte
+
+        message << "#{length}#{type}"
+        message << @pipe.read(length)
+
+        decoded = OTNetstring.parse(message)
+        modified_paths = decoded["events"].map {|event| event["path"]}
+        # passing the full info as a second block param feels icky, but such is
+        # the trap of backward compatibility.
+        callback.call(modified_paths, decoded)
       end
     end
   rescue Interrupt, IOError, Errno::EBADF
@@ -110,7 +131,7 @@ class FSEvent
   private
 
   def parse_options(options={})
-    opts = []
+    opts = ['--format=otnetstring']
     opts.concat(['--since-when', options[:since_when]]) if options[:since_when]
     opts.concat(['--latency', options[:latency]]) if options[:latency]
     opts.push('--no-defer') if options[:no_defer]
